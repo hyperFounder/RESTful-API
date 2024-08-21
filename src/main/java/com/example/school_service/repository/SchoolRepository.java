fffb9910996a5244cbbe2a76be58081dfacc91db
Ryan Ribeiro <rxr274@student.bham.ac.uk>
2024-08-21 12:43:37 +0100

    create the school repository interface

---
+++ src/main/java/com/example/school_service/repository/SchoolRepository.java
@@ -0,0 +1,10 @@
+package com.example.school_service.repository;
+
+import com.example.school_service.model.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+}
