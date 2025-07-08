5d4d8acaf2ff138aecd8229bfa553800342d111c
Ryan Ribeiro <117395661+hyperFounder@users.noreply.github.com>
2025-07-08 16:36:09 +0100

    Update SchoolRepository.java

--- src/main/java/com/example/school_service/repository/SchoolRepository.java
+++ src/main/java/com/example/school_service/repository/SchoolRepository.java
@@ -1,4 +1,4 @@
-package com.example.school_service.repository;
+package com.example.school_service.repository; 
 
 import com.example.school_service.model.School;
 import org.springframework.data.jpa.repository.JpaRepository;
